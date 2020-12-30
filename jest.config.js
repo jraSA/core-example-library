afc8139986cf9d35d30df2259fcb04010feada01
Jorge Rodriguez <jorge.rodriguez@piksel.com>
2020-12-30 18:13:13 +0100

    modified jest.config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/lib/'],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 70,
+      lines: 80,
+      functions: 79,
+    },
+  },
 };
